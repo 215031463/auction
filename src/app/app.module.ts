86aecdd1e38eb5ff654d8d24a955b663aa002be5
215031463@qq.com <18580586165@163.com>
2017-06-14 21:17:48 +0800

    prepare start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    FooterComponent,
+    SearchComponent,
+    ProductComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
